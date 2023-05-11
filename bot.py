deaead9cfa40a4d35f347db7fc84750225c8f40c
vkdnvkjdnvk <115922483+vkdnvkjdnvk@users.noreply.github.com>
2023-05-11 20:16:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ from handlers.save_media import (
 MediaList = {}
 
 Bot = Client(
-    name=Config.BOT_USERNAME,
+    
     in_memory=True,
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
